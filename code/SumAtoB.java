8de65413207dca20cae81f83bf988976000cf82e
lowgiant <lowgiant@gmail.com>
2022-02-06 23:55:13 +0900

    feat: SumAtoB

---
+++ code/SumAtoB.java
@@ -0,0 +1,15 @@
+
+public class SumAtoB{
+
+	public static long solution(int a, int b) {
+		return sumAtoB(Math.min(a, b), Math.max(b, a));
+	}
+
+	private static long sumAtoB(long a, long b) {
+		return (b - a + 1) * (a + b) / 2;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(solution(3,5));
+	}
+}
