159c98bf90f5b058032584d4a3595222c4c7df01
lowgiant <lowgiant@gmail.com>
2022-02-13 23:38:50 +0900

    feat: Add StrChangeInt

---
+++ code/StrChangeInt.java
@@ -0,0 +1,9 @@
+public class StrChangeInt{
+	public static int strChangeInt(String s){
+		return Integer.parseInt(s);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(strChangeInt("1234"));
+	}
+}
