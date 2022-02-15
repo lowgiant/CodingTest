34f6d4d0ff966bf3010c302424a316510d1b61ed
lowgiant <lowgiant@gmail.com>
2022-02-15 23:24:37 +0900

    feat: Add DigitAdd

---
+++ code/DigitAdd.java
@@ -0,0 +1,18 @@
+public class DigitAdd {
+
+	public static int digitAdd(int n) {
+		int answer = 0;
+
+		while (n != 0) {
+			answer += n % 10;
+			n /= 10;
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(digitAdd(123));
+		System.out.println(digitAdd(987));
+	}
+}
